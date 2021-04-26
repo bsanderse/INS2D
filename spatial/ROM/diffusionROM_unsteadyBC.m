44700d132873c34c3ff46874ef59234deae56c02
Rosenberger <h.k.e.rosenberger@student.tue.nl>
2021-04-26 11:32:56 +0200

    fixed error in offline decomp of diffusion: BC has to be described by two modes

--- spatial/ROM/diffusionROM_unsteadyBC.m
+++ spatial/ROM/diffusionROM_unsteadyBC.m
@@ -7,6 +7,8 @@ M = options.rom.M;
 % Nv = options.grid.Nv;
 
 abc = options.rom.abc;
+abc1 = options.rom.abc1;
+abc2 = options.rom.abc2;
 
 Diff  = options.rom.Diff;
 DiffBC  = options.rom.DiffBC;
@@ -21,7 +23,8 @@ norm(vbc1-vbc2)
 switch visc
     
     case 'laminar'
-        d2     = Diff*R + DiffBC*abc(t) + yDiff*abc(t);
+%         d2     = Diff*R + DiffBC*abc(t) + yDiff*abc(t);
+        d2     = Diff*R + DiffBC*abc(t) + yDiff(:,1)*abc1(t) + yDiff(:,2)*abc2(t);
 %         d2     = Diff*R; %pfusch
         
         if (getJacobian == 1)
@@ -32,6 +35,25 @@ switch visc
         error('turbulent diffusion not implemented for ROM');
         
 end
+
+Diffu  = options.discretization.Diffu;
+Diffv  = options.discretization.Diffv;
+yDiffu = options.discretization.yDiffu;
+yDiffv = options.discretization.yDiffv;
+
+D_h = blkdiag(Diffu, Diffv);
+y_D = [yDiffu; yDiffv];
+
+B   = options.rom.B;
+
+V_r = getFOM_velocity(R,t,options);
+norm(B'*(D_h*V_r+y_D)-d2)
+norm(B'*D_h*B-Diff)
+norm(B'*D_h*get_unsteadyVbc(t,options)-DiffBC*abc(t))
+% norm(B'*D_h*y_D-yDiff*abc(t))
+% norm(B'*y_D-(yDiff*abc(t)))
+norm(B'*y_D-(yDiff(:,1)*abc1(t) + yDiff(:,2)*abc2(t)))
+
 
 end
 
--- spatial/ROM/operator_rom_diffusion_unsteadyBC.m
+++ spatial/ROM/operator_rom_diffusion_unsteadyBC.m
@@ -17,39 +17,51 @@ M1  = size(P,1);
 
 % Phi^T(D_h(Phi a + Phi_bc a_bc) + y_D) = Diff a + DiffBC a_bc +
 % y_Diff0
-Diff = zeros(M1,M);
-DiffBC = zeros(M1,Mbc);
 
-[d2u_0,d2v_0] = mydiffusion(Z,0,options,0);
-yDiff0        = P*[d2u_0;d2v_0];
-
-for i=1:M
-    [d2u,d2v] = mydiffusion(B(:,i),0,options,0);
-    Diff(:,i) = P*[d2u;d2v] - yDiff0;
-end
-
-for i=1:Mbc
-    [d2u,d2v] = mydiffusion(Vbc0(:,i),0,options,0);
-    DiffBC(:,i) = P*[d2u;d2v] - yDiff0;
-end
+% Diff = zeros(M1,M);
+% DiffBC = zeros(M1,Mbc);
+% 
+% [d2u_0,d2v_0] = mydiffusion(Z,0,options,0);
+% yDiff0        = P*[d2u_0;d2v_0];
+% 
+% for i=1:M
+%     [d2u,d2v] = mydiffusion(B(:,i),0,options,0);
+%     Diff(:,i) = P*[d2u;d2v] - yDiff0;
+% end
+% 
+% for i=1:Mbc
+%     [d2u,d2v] = mydiffusion(Vbc0(:,i),0,options,0);
+%     DiffBC(:,i) = P*[d2u;d2v] - yDiff0;
+% end
 
 %% alternative
 
 Diffu  = options.discretization.Diffu;
 Diffv  = options.discretization.Diffv;
-yDiffu = options.discretization.yDiffu;
-yDiffv = options.discretization.yDiffv;
 
 Diff_   = P*blkdiag(Diffu, Diffv)*B;
 DiffBC_ = P*blkdiag(Diffu, Diffv)*Vbc0;
+
+yDiffu1 = options.bc_options1.discretization.yDiffu;
+yDiffv1 = options.bc_options1.discretization.yDiffv;
+yDiff0_1 = P*[yDiffu1; yDiffv1];
+
+yDiffu2 = options.bc_options2.discretization.yDiffu;
+yDiffv2 = options.bc_options2.discretization.yDiffv;
+yDiff0_2 = P*[yDiffu2; yDiffv2];
+
+yDiffu = options.discretization.yDiffu;
+yDiffv = options.discretization.yDiffv;
 yDiff0_ = P*[yDiffu; yDiffv];
 
-norm(Diff-Diff_)
-norm(DiffBC-DiffBC_)
-norm(yDiff0 - yDiff0_)
+norm(yDiff0_ - (yDiff0_1*options.rom.abc1(0)+yDiff0_2*options.rom.abc2(0)))
+
+% norm(Diff-Diff_)
+% norm(DiffBC-DiffBC_)
+% norm(yDiff0 - (yDiff0_1*options.rom.abc1(0)+yDiff0_2*options.rom.abc2(0)))
 
 Diff   = Diff_;
 DiffBC = DiffBC_;
-yDiff0 = yDiffy0_;
+yDiff0 = [yDiff0_1, yDiff0_2];
 
 
