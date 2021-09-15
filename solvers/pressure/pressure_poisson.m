f22204e5ec4b957eeec29654d6ce3f48446e5693
Benjamin Sanderse <30893600+bsanderse@users.noreply.github.com>
2021-09-15 15:15:20 +0200

    improved speed of Poisson equation using decomposition()

--- solvers/pressure/pressure_poisson.m
+++ solvers/pressure/pressure_poisson.m
@@ -27,10 +27,11 @@ end
 % choose a method to solve the Poisson equation
 if (poisson==1)
     % using pre-determined LU decomposition
-    L   = options.solversettings.L;
-    U   = options.solversettings.U;
-    b   = L\f;
-    dp  = U\b;
+%     L   = options.solversettings.L;
+%     U   = options.solversettings.U;
+%     b   = L\f;
+%     dp  = U\b;
+    dp  = options.solversettings.decomp\f;
 elseif (poisson==2)
     % using preconditioned CG
     L   = options.solversettings.L;
