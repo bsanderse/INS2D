c7db017ea3328f0a3323f333a57913194a799877
Rosenberger <h.k.e.rosenberger@student.tue.nl>
2022-01-05 16:27:08 +0100

    complete complete efficient convection diag computation + tests

--- case_files/shear_layer_ROM/shear_layer_ROM_BCtype.m
+++ case_files/shear_layer_ROM/shear_layer_ROM_BCtype.m
@@ -10,14 +10,14 @@ function BC = shear_layer_ROM_BCtype
     % left/right: x-direction
     % low/up: y-direction
 
-    BC.u.left  = 'mvp-obc';%'mvp-obc';   % valid options: dir, per, pres 
-    BC.u.right = 'mvp-obc';  % valid options: dir, per, pres
-    BC.u.low   = 'dir';   % valid options: dir, per, sym
+    BC.u.left  = 'per';%'mvp-obc';   % valid options: dir, per, pres 
+    BC.u.right = 'per';  % valid options: dir, per, pres
+    BC.u.low   = 'mvp-obc';   % valid options: dir, per, sym
     BC.u.up    = 'dir';   % valid options: dir, per, sym
 
-    BC.v.left  = 'mvp-obc';   % valid options: dir, per, sym
-    BC.v.right = 'mvp-obc';   % valid options: dir, per, sym
-    BC.v.low   = 'dir';   % valid options: dir, per, pres
+    BC.v.left  = 'per';   % valid options: dir, per, sym
+    BC.v.right = 'per';   % valid options: dir, per, sym
+    BC.v.low   = 'mvp-obc';   % valid options: dir, per, pres
     BC.v.up    = 'dir';   % valid options: dir, per, pres
 
 %     BC.gO = @(u) 1; % non-negative function, dissipation model %% maybe move to parameters
