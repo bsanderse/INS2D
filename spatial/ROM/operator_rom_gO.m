9db55807b454559a1697cd8025a5c2b88c03ac50
hker <hker@cwi.nl>
2022-02-08 13:53:19 +0100

    make gO fidelity-dependent, not test yet

--- spatial/ROM/operator_rom_gO.m
+++ spatial/ROM/operator_rom_gO.m
@@ -8,13 +8,18 @@ id_normal = options.grid.id_normal;
 id_tangential = options.grid.id_tangential;
 id_n_t = id_normal+id_tangential;
 
-gO_hom = factor*P*diag(id_n_t)*B;
+warning('assuming homogeneous grid')
+hx = options.grid.hx(1);
+hy = options.grid.hy(1);
+outward_faces = [hx*ones(Nu,1); hy*ones(Nv,1)];
+
+gO_hom = factor*P*diag(outward_faces)*diag(id_n_t)*B;
 
 
 if options.rom.rom_bc == 2
     if options.rom.bc_recon == 3
 phi_inhom = options.rom.phi_inhom;
-gO_inhom = factor*P*diag(id_n_t)*phi_inhom;
+gO_inhom = factor*P*diag(outward_faces)*diag(id_n_t)*phi_inhom;
     else
         error('Sorry, obc offline decomposition not implemented for rom_bc == 2, but bc_recon =/=3')
     end
