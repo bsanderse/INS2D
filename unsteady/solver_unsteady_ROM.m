616aaa0987b47bfde0802e661aea57682730706d
Benjamin Sanderse <bsanderse@gmail.com>
2022-03-24 21:36:18 +0100

    ROM initial condition 'divergence free'
    * for the case of a velocity-pressure ROM

--- unsteady/solver_unsteady_ROM.m
+++ unsteady/solver_unsteady_ROM.m
@@ -629,6 +629,33 @@ V_orig = V;
 % get the coefficients of the ROM
 R = getROM_velocity(V,t,options);
 
+
+if (options.rom.div_free == 0)
+    % get initial ROM pressure by projecting initial presssure
+    q = getROM_pressure(p,t,options);
+    
+    % make initial condition 'divergence free'
+    f  = options.rom.Mdiv*R + options.rom.yMt(:,1);
+    dq = pressure_poisson_ROM(f,t,options);
+    R  = R - options.rom.G*dq;
+    
+elseif (options.rom.div_free == 1)
+    if (options.rom.pressure_recovery == 1)
+        % get initial pressure that corresponds to the ROM velocity field
+        q = pressure_additional_solve_ROM(R,t,options);
+        p = getFOM_pressure(q,t,options);
+
+        % overwrite the arrays with total solutions
+        if (save_unsteady == 1)
+            p_total(n,:)  = p;
+        end
+    else
+        % q is not used, set to arbitrary value
+        q = 0;
+    end
+end
+
+
 if (options.rom.process_iteration_FOM == 1)
     % map back to velocity space to get statistics of initial velocity field
     % note that V will not be equal to the specified initial field, because
@@ -646,25 +673,7 @@ if (options.rom.process_iteration_FOM == 1)
 end
 
 
-if (options.rom.div_free == 0)
-    % get initial ROM pressure by projecting initial presssure
-    q = getROM_pressure(p,t,options);
-    
-elseif (options.rom.div_free == 1)
-    if (options.rom.pressure_recovery == 1)
-        % get initial pressure that corresponds to the ROM velocity field
-        q = pressure_additional_solve_ROM(R,t,options);
-        p = getFOM_pressure(q,t,options);
 
-        % overwrite the arrays with total solutions
-        if (save_unsteady == 1)
-            p_total(n,:)  = p;
-        end
-    else
-        % q is not used, set to arbitrary value
-        q = 0;
-    end
-end
 
 
 %% reduced order solution tests
