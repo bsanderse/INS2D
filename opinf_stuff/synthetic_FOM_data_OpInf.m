301e67a63a0896060b54bc66162c82c9f590e775
hker <hker@cwi.nl>
2024-05-02 16:45:28 +0200

    implement synthetic FOM data production

--- opinf_stuff/synthetic_FOM_data_OpInf.m
+++ opinf_stuff/synthetic_FOM_data_OpInf.m
@@ -1,7 +1,7 @@
 % try operator inference in INS2D framework but with synthetic data
-function [D_OpInf,C_OpInf] = synthetic_data_OpInf(nu,D,C)
+function [D_OpInf,C_OpInf] = synthetic_FOM_data_OpInf(nu,D,C,phi)
 
-r = size(D,1);
+[N,r] = size(phi);
 
 %% 
 % Us = zeros(r);
@@ -15,23 +15,21 @@ r = size(D,1);
 
 %%
 
-sparse_ = false;
+sparse_ = true;
 K = 2;
-if sparse_
-    Uhats = sparse(r+r^2,r*r*K);
-    RHSs = sparse(r,r*r*K);
-else
+% if sparse_
+%     Uhats = sparse(r+r^2,r*r*K);
+%     RHSs = sparse(r,r*r*K);
+% else
     Uhats = zeros(r+r^2,r,r,K);
     RHSs = zeros(r,r,r,K);
-end
+% end
 
 for k=1:K
     for i=1:r
-        U = zeros(r,1);
-        U(i) = k;
+        U = sparse(i,1,k,N,1);
         for j =1:r
-            V = zeros(r,1);
-            V(j) = k;
+            V = sparse(j,1,k,N,1);
             %% heterogenous data
             % VU = kron(V,U); 
             % Uhats(:,i,j,k) = [U; VU];
@@ -39,21 +37,18 @@ for k=1:K
             %% homogeneous data
             U = U + V;
             UU = kron(U,U);
-            if sparse_ 
-                Uhats(:,i+(j-1)*r+(k-1)*K) = [U; UU];
-                RHSs(:,i+(j-1)*r+(k-1)*K) = nu*D*U - C*UU;
-            else
-                Uhats(:,i,j,k) = [U; UU];
-                RHSs(:,i,j,k) = nu*D*U - C*UU;
-            end
+            a = phi'*U;
+            Uhats(:,i,j,k) = [a;kron(a,a)];
+            RHSs(:,i,j,k) = phi'*(nu*D*U - C*UU);
             %%
         end
     end
-    end
-    if ~sparse_
+end
+
+    % if ~sparse_
         Uhats = Uhats(:,:);
         RHSs = RHSs(:,:);
-    end
+    % end
 
 %%
 
