d769d2bcd3a35853b5b403c7d6f592bdba8bbdc3
hker <hker@cwi.nl>
2024-07-11 10:59:20 +0200

    implement vec2ops and ops2vec
    methods to switch between vectorization and native representation of diffusion-convection concatenated matrix

--- opinf_stuff/vec2ops.m
+++ opinf_stuff/vec2ops.m
@@ -2,4 +2,9 @@ function [D,C] = vec2ops(vec)
 
 num_el = @(r) r*(r+r^2);
 
-CONTINUE!
+M = round(fmincon(@(r) norm(num_el(r) - numel(vec)),1));
+
+O = reshape(vec,M+M^2,M)';
+
+D = O(:,1:M);
+C = O(:,M+1:end);
