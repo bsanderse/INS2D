ff3bf2ddf44aee25bb5c929890f4d0b4af864dc3
hker <hker@cwi.nl>
2024-09-09 16:27:19 +0200

    before reproducing B's article test case

--- opinf_stuff/update_convection.m
+++ opinf_stuff/update_convection.m
@@ -16,6 +16,8 @@ A_vecT_ = A_vecT(A_kron',feasible_basis);
 
 
 % SVD approach
+% note: to improve efficiency, this SVD could be precomputed/ performed 
+% once outside the CVX SDP solve
 c = lsq_SVD(A_vecT_,B_vec);
 
 
