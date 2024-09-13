10a4ab0804af1de7d4486fc9533feafe33bbe549
hker <hker@cwi.nl>
2024-09-13 19:05:21 +0200

    start implementing opinf snapshot preprocessing

---
+++ opinf_stuff/get_opinf_snapshots.m
@@ -0,0 +1,5 @@
+function [A,A_dot] = get_opinf_snapshots(V_svd)
+% extend this function by adding inputs such as:
+% - exact FOM time derivative snapshots
+% - specification, what kind of time derivative approximation should be
+% used
