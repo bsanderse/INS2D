47297f8c7f0c82ab6dc7852a3c54acee691307ec
Henrik Rosenberger <hker@cwi.nl>
2024-09-16 11:02:52 +0200

    implement opinf snapshot data preprocessing

--- opinf_stuff/get_opinf_snapshots.m
+++ opinf_stuff/get_opinf_snapshots.m
@@ -1,5 +1,10 @@
-function [A,A_dot] = get_opinf_snapshots(V_svd)
+function [A,A_dot] = get_opinf_snapshots(A_raw)
 % extend this function by adding inputs such as:
 % - exact FOM time derivative snapshots
 % - specification, what kind of time derivative approximation should be
 % used
+
+A_dot = (A_raw(:,2:end,:) - A_raw(:,1:end-1,:))/dt;
+
+A = A_raw(:,1:end-1,:); % explict Euler
+% A = A_raw(:,2:end,:); % implicit Euler
