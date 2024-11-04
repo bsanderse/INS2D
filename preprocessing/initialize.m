6d03cb484c0380b461e287d8c7e864a037fcd9c3
hker <hker@cwi.nl>
2024-11-04 18:25:59 +0100

    allow custom-named initial condition file

--- preprocessing/initialize.m
+++ preprocessing/initialize.m
@@ -1,15 +1,17 @@
-file_name = [options.case.project '_IC'];
+if ~exist(IC_file,'var')
+    IC_file = [options.case.project '_IC'];
+end
 
-if (exist(file_name,'file'))
+if (exist(IC_file,'file'))
     
     t       = options.time.t_start;
-    IC      = str2func(file_name);    
+    IC      = str2func(IC_file);    
     [u_start,v_start,p_start,options] = IC(t,options);
     
     V_start = [u_start(:);v_start(:)];
 
 else
     
-    error(['initial condition file ' file_name ' not available']);
+    error(['initial condition file ' IC_file ' not available']);
     
 end
